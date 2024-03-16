296be6e97d99bf66d3f2962bd979298bfe09a835
MathSena <MathSena07@hotmail.com>
2024-03-16 00:39:34 +0000

    Colocando feign no serviço

--- ms-reservas/src/main/java/br/com/fiap/hackaton/ms/reservas/client/ClienteClient.java
+++ ms-reservas/src/main/java/br/com/fiap/hackaton/ms/reservas/client/ClienteClient.java
@@ -8,23 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "cliente-service", url = "http://localhost:8081/v1/clientes")
+@FeignClient(name = "cliente-service", url = "http://localhost:8080/v1/clientes")
 public interface ClienteClient {
 
-  @PostMapping
-  ResponseEntity<Cliente> createCliente(@RequestBody ClienteRecord clienteRecord);
 
-  @GetMapping
-  ResponseEntity<Page<ClienteRecord>> findAllClientes(
-      @RequestParam("page") Integer page, @RequestParam("size") Integer size);
+    @PostMapping
+    ResponseEntity<Cliente> createCliente(@RequestBody ClienteRecord clienteRecord);
 
-  @GetMapping("/{id}")
-  ResponseEntity<Cliente> findClienteById(@PathVariable("id") Long id);
+    @GetMapping
+    ResponseEntity<Page<ClienteRecord>> findAllClientes(
+            @RequestParam("page") Integer page, @RequestParam("size") Integer size);
 
-  @DeleteMapping("/{id}")
-  ResponseEntity<Void> deleteCliente(@PathVariable("id") Long id);
+    @GetMapping("/{id}")
+    ResponseEntity<Cliente> findClienteById(@PathVariable("id") Long id);
 
-  @PutMapping("/{id}")
-  ResponseEntity<Cliente> updateCliente(
-      @PathVariable("id") Long id, @RequestBody ClienteRecord clienteRecord);
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteCliente(@PathVariable("id") Long id);
+
+    @PutMapping("/{id}")
+    ResponseEntity<Cliente> updateCliente(
+            @PathVariable("id") Long id, @RequestBody ClienteRecord clienteRecord);
+
 }
